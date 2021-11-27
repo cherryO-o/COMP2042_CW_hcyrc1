5a63b0ca286554ff473eda592ab1001f6be10404
RuiQi <ruiqi1611@gmail.com>
2021-11-27 17:33:25 +0800

    Created interface Screen

---
+++ src/GUI/Screen.java
@@ -0,0 +1,10 @@
+package GUI;
+
+import java.awt.*;
+
+interface Screen {
+    void drawScreen(Graphics2D g2d);
+    void drawContainer(Graphics2D g2d);
+    void drawText(Graphics2D g2d);
+    void drawButton(Graphics2D g2d);
+}
