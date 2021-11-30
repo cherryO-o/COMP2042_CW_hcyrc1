343f4c4c5a3074729e0992401fe0de5f5bca1541
RuiQi <ruiqi1611@gmail.com>
2021-11-30 19:15:53 +0800

    JUnit tests for Wall class

---
+++ src/Class Testing/Components/WallTest.java
@@ -0,0 +1,34 @@
+package Components;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WallTest {
+
+    @Test
+    void getBrickCount() {
+        int count = 5;
+        int brickCount;
+        Wall.setBrickCount(count);
+        brickCount = Wall.getBrickCount();
+        assertEquals(count, brickCount);
+    }
+
+    @Test
+    void isBallLost() {
+        boolean Lost = true;
+        boolean ans;
+        Wall.ballLost(Lost);
+        ans = Wall.isBallLost();
+        assertEquals(Lost,ans);
+    }
+
+    @Test
+    void resetBallCount() {
+        int ballCount;
+        Wall.resetBallCount();
+        ballCount = Wall.getBallCount();
+        assertEquals(3,ballCount);
+    }
+}
