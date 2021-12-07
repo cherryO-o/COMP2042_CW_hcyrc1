9e3e3a316f3e143cb1d3fd91e16eeaf5b641677b
RuiQi <ruiqi1611@gmail.com>
2021-12-07 13:54:25 +0800

    Created a factory using factory design pattern for makeBrick() method

---
+++ src/Components/MakeBrickFactory.java
@@ -0,0 +1,19 @@
+package Components;
+
+import java.awt.*;
+
+public class MakeBrickFactory {
+    public final int CLAY = 1;
+    public final int STEEL = 2;
+    public final int CEMENT = 3;
+
+    public Brick makeBrick(Point point, Dimension size, int type){
+        Brick out = switch (type) {
+            case CLAY -> new ClayBrick(point, size);
+            case STEEL -> new SteelBrick(point, size);
+            case CEMENT -> new CementBrick(point, size);
+            default -> throw new IllegalArgumentException(String.format("Unknown Type:%d\n", type));
+        };
+        return  out;
+    }
+}
