9abed9b2d848e9d44184c2866117dc54d20f3303
RuiQi <ruiqi1611@gmail.com>
2021-12-07 21:25:01 +0800

    added new brick variable

--- src/Components/MakeBrickFactory.java
+++ src/Components/MakeBrickFactory.java
@@ -6,12 +6,14 @@ public class MakeBrickFactory {
     public final int CLAY = 1;
     public final int STEEL = 2;
     public final int CEMENT = 3;
+    public final int SPEED = 4;
 
     public Brick makeBrick(Point point, Dimension size, int type){
         Brick out = switch (type) {
             case CLAY -> new ClayBrick(point, size);
             case STEEL -> new SteelBrick(point, size);
             case CEMENT -> new CementBrick(point, size);
+            case SPEED -> new SpeedBrick(point, size);
             default -> throw new IllegalArgumentException(String.format("Unknown Type:%d\n", type));
         };
         return  out;
