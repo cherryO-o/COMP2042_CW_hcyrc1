040914a7f6789ebbe1e0d446a21d65885d89f3f0
RuiQi <ruiqi1611@gmail.com>
2021-12-08 23:25:48 +0800

    JUnit test on MakeBrickFactory class

--- src/Components/MakeBrickFactory.java
+++ src/Components/MakeBrickFactory.java
@@ -2,13 +2,32 @@ package Components;
 
 import java.awt.*;
 
+/**
+ * The brick factory where different brick types are referenced
+ */
 public class MakeBrickFactory {
     public final int CLAY = 1;
     public final int STEEL = 2;
     public final int CEMENT = 3;
     public final int SPEED = 4;
 
-    public Brick makeBrick(Point point, Dimension size, int type){
+    private Dimension size;
+
+    /**
+     * MakeBrickFactory constructor
+     * @param size = gets size of brick
+     */
+    public MakeBrickFactory(Dimension size) {
+        this.size = size;
+    }
+
+    /**
+     * The brick factory where different brick types objects are made
+     * @param point = location of brick
+     * @param type = type of brick
+     * @return
+     */
+    public Brick makeBrick(Point point, int type){
         Brick out = switch (type) {
             case CLAY -> new ClayBrick(point, size);
             case STEEL -> new SteelBrick(point, size);
@@ -16,6 +35,6 @@ public class MakeBrickFactory {
             case SPEED -> new SpeedBrick(point, size);
             default -> throw new IllegalArgumentException(String.format("Unknown Type:%d\n", type));
         };
-        return  out;
+        return out;
     }
 }
