643cd85cde481972aa709baf287977863c4df75a
RuiQi <ruiqi1611@gmail.com>
2021-12-08 23:33:43 +0800

    Javadocs on SpeedBrick class

--- src/Components/SpeedBrick.java
+++ src/Components/SpeedBrick.java
@@ -2,16 +2,18 @@ package Components;
 
 import java.awt.*;
 import java.awt.Point;
-import java.awt.geom.Point2D;
 
+/**
+ * Newly created brick type
+ */
 public class SpeedBrick extends Brick{
 
     private static final String NAME = "Speed Brick";
     private static final Color DEF_INNER = new Color(102,178,225).darker();
     private static final Color DEF_BORDER = Color.GRAY;
     private static final int SPEED_STRENGTH = 1;
-    private int speedX = 3;
-    private int speedY = -3;
+    private int speedX = 2;
+    private int speedY = -2;
 
     public SpeedBrick(Point point, Dimension size){
         super(NAME,point,size,DEF_BORDER,DEF_INNER,SPEED_STRENGTH);
@@ -27,6 +29,9 @@ public class SpeedBrick extends Brick{
         return super.brickFace;
     }
 
+    /**
+     * Increases speed of ball upon impact on this brick
+     */
     public void impact(){
         super.impact();
         speedX += 1;
