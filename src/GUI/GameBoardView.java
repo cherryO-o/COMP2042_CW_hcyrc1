0b620fdbb3cf46655d7021fe5e3cec47e89b3573
RuiQi <ruiqi1611@gmail.com>
2021-12-08 18:57:25 +0800

    Created new class View for GameBoard using MVC

---
+++ src/GUI/GameBoardView.java
@@ -0,0 +1,244 @@
+package GUI;
+import Components.*;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.font.FontRenderContext;
+
+import Components.CountDown;
+import Components.Wall;
+import Controller.DebugConsole;
+
+/**
+ * This class is the View of GameBoard
+ */
+public class GameBoardView extends JComponent {
+
+    private static final String CONTINUE = "Continue";
+    private static final String RESTART = "Restart";
+    private static final String EXIT = "Exit";
+    private static final String PAUSE = "Pause Menu";
+    private static final int TEXT_SIZE = 30;
+    private static final Color MENU_COLOR = new Color(255,255,255);
+
+    public static final int DEF_WIDTH = 600;
+    public static final int DEF_HEIGHT = 450;
+
+    private static final Color BG_COLOR = Color.WHITE;
+
+    public CountDown countDown = new CountDown();
+
+    public Wall wall;
+
+    public String message;
+    public String heart;
+    public String message2;
+    public String scoreCount;
+
+    public boolean showPauseMenu;
+
+    private Font menuFont;
+
+    public Rectangle continueButtonRect;
+    public Rectangle exitButtonRect;
+    public Rectangle restartButtonRect;
+    public int strLen;
+
+    public DebugConsole debugConsole;
+
+    public GameBoardView(JFrame owner) {
+        super();
+
+        strLen = 0;
+        showPauseMenu = false;
+
+        menuFont = new Font("Monospaced", Font.PLAIN, TEXT_SIZE);
+
+
+        //this.initialize();
+        message = "";
+        message2 = "";
+        scoreCount = "";
+        wall = new Wall(new Rectangle(0, 0, DEF_WIDTH, DEF_HEIGHT), 30, 3, 6 / 2, new Point(300, 430));
+
+        debugConsole = new DebugConsole(owner, wall, this);
+        //initialize the first level
+        wall.nextLevel();
+    }
+
+    /**
+     * made wall.ball, wall.bricks, wall.player public
+     * @param g
+     */
+    public void paint(Graphics g){
+
+        Graphics2D g2d = (Graphics2D) g;
+
+        clear(g2d);
+
+        g2d.setColor(Color.BLUE);
+        g2d.drawString(message,250,225);
+        g2d.drawString(message2,285,205);
+        g2d.drawString(scoreCount, 270,240);
+
+        drawBall(wall.ball,g2d);
+
+        for(Brick b : wall.bricks)
+            if(!b.isBroken())
+                drawBrick(b,g2d);
+
+        drawPlayer(wall.player,g2d);
+
+        if(showPauseMenu)
+            drawMenu(g2d);
+
+        Toolkit.getDefaultToolkit().sync();
+    }
+
+    private void clear(Graphics2D g2d){
+        Color tmp = g2d.getColor();
+        g2d.setColor(BG_COLOR);
+        g2d.fillRect(0,0,getWidth(),getHeight());
+        g2d.setColor(tmp);
+    }
+
+    private void drawBrick(Brick brick,Graphics2D g2d){
+        Color tmp = g2d.getColor();
+
+        g2d.setColor(brick.getInnerColor());
+        g2d.fill(brick.getBrick());
+
+        g2d.setColor(brick.getBorderColor());
+        g2d.draw(brick.getBrick());
+
+
+        g2d.setColor(tmp);
+    }
+
+    private void drawBall(Ball ball, Graphics2D g2d){
+        Color tmp = g2d.getColor();
+
+        Shape s = ball.getBallFace();
+
+        g2d.setColor(ball.getInnerColor());
+        g2d.fill(s);
+
+        g2d.setColor(ball.getBorderColor());
+        g2d.draw(s);
+
+        g2d.setColor(tmp);
+    }
+
+    private void drawPlayer(Player p, Graphics2D g2d){
+        Color tmp = g2d.getColor();
+
+        Shape s = p.getPlayerFace();
+        g2d.setColor(Player.INNER_COLOR);
+        g2d.fill(s);
+
+        g2d.setColor(Player.BORDER_COLOR);
+        g2d.draw(s);
+
+        g2d.setColor(tmp);
+    }
+
+    private void drawMenu(Graphics2D g2d){
+        obscureGameBoard(g2d);
+        drawPauseMenu(g2d);
+    }
+
+    private void obscureGameBoard(Graphics2D g2d){
+
+        Composite tmp = g2d.getComposite();
+        Color tmpColor = g2d.getColor();
+
+        AlphaComposite ac = AlphaComposite.getInstance(AlphaComposite.SRC_OVER,0.55f);
+        g2d.setComposite(ac);
+
+        g2d.setColor(Color.BLACK);
+        g2d.fillRect(0,0,DEF_WIDTH,DEF_HEIGHT);
+
+        g2d.setComposite(tmp);
+        g2d.setColor(tmpColor);
+    }
+
+    private void drawPauseMenu(Graphics2D g2d){
+        Font tmpFont = g2d.getFont();
+        Color tmpColor = g2d.getColor();
+
+
+        g2d.setFont(menuFont);
+        g2d.setColor(MENU_COLOR);
+
+        if(strLen == 0){
+            FontRenderContext frc = g2d.getFontRenderContext();
+            strLen = menuFont.getStringBounds(PAUSE,frc).getBounds().width;
+        }
+
+        int x = (this.getWidth() - strLen) / 2;
+        int y = this.getHeight() / 10;
+
+        g2d.drawString(PAUSE,x,y);
+
+        x = this.getWidth() / 8;
+        y = this.getHeight() / 4;
+
+
+        if(continueButtonRect == null){
+            FontRenderContext frc = g2d.getFontRenderContext();
+            continueButtonRect = menuFont.getStringBounds(CONTINUE,frc).getBounds();
+            continueButtonRect.setLocation(x,y-continueButtonRect.height);
+        }
+
+        g2d.drawString(CONTINUE,x,y);
+
+        y *= 2;
+
+        if(restartButtonRect == null){
+            restartButtonRect = (Rectangle) continueButtonRect.clone();
+            restartButtonRect.setLocation(x,y-restartButtonRect.height);
+        }
+
+        g2d.drawString(RESTART,x,y);
+
+        y *= 3.0/2;
+
+        if(exitButtonRect == null){
+            exitButtonRect = (Rectangle) continueButtonRect.clone();
+            exitButtonRect.setLocation(x,y-exitButtonRect.height);
+        }
+
+        g2d.drawString(EXIT,x,y);
+
+
+
+        g2d.setFont(tmpFont);
+        g2d.setColor(tmpColor);
+    }
+
+    /**
+     * Let ballCount be represented by emojis instead of integers
+     * @return heart (emoji)
+     */
+    public String Lives(){
+        heart = "";
+        for(int a = 0; a < wall.getBallCount(); a++)
+            heart += new String(Character.toChars(0x1F497));
+        return heart;
+    }
+
+    /**
+     * Prints out player's score for every level
+     */
+    public void printHighScore() {
+        StringBuilder text = new StringBuilder();
+
+        for(int i = 0; i<6; i++) {
+            text.append("\n" + wall.levelScore[i]);
+        }
+
+        ImageIcon icon = new ImageIcon("yay.png");
+        JOptionPane.showMessageDialog(null, "Your Score: " + text, "High Score", JOptionPane.PLAIN_MESSAGE, icon);
+    }
+
+}
