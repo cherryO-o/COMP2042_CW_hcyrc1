481c184e712e8734b3524fb2a7c528840810aea6
RuiQi <ruiqi1611@gmail.com>
2021-12-10 12:59:17 +0800

    Moved from GUI to Controller package

---
+++ src/Controller/GameFrame.java
@@ -0,0 +1,142 @@
+/*
+ *  Brick Destroy - A simple Arcade video game
+ *   Copyright (C) 2017  Filippo Ranza
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package Controller;
+
+import Controller.GameBoardController;
+import GUI.GameBoardView;
+import GUI.HomeMenu;
+import GUI.Instructions;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowFocusListener;
+
+/**
+ * Updates which screen to appear and disappear
+ */
+public class GameFrame extends JFrame implements WindowFocusListener {
+
+    private static final String DEF_TITLE = "Brick Destroy";
+
+    private HomeMenu homeMenu;
+    private Instructions instructions;
+    private GameBoardView view;
+    private GameBoardController controller;
+
+    private boolean gaming;
+
+    public GameFrame(){
+        super();
+
+        gaming = false;
+
+        this.setLayout(new BorderLayout());
+
+        view = new GameBoardView(this);
+
+        controller = new GameBoardController(this, view);
+
+        homeMenu = new HomeMenu(this,new Dimension(550, 432));
+
+        instructions = new Instructions(this,new Dimension(550, 432));
+
+        this.add(homeMenu,BorderLayout.CENTER);
+
+        this.setUndecorated(true);
+
+
+    }
+
+    /**
+     * Enables screens to be visible
+     */
+    public void initialize(){
+        this.setTitle(DEF_TITLE);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.pack();
+        this.autoLocate();
+        this.setVisible(true);
+    }
+
+    /**
+     * Enables instruction screen to appear
+     */
+    public void enableInstruction(){
+        this.dispose();
+        this.remove(homeMenu);
+        this.add(instructions,BorderLayout.CENTER);
+        this.setUndecorated(true);
+        initialize();
+        this.addWindowFocusListener(this);
+    }
+
+    /**
+     * Enables game levels screen to appear
+     */
+    public void enableGameBoard(){
+        this.dispose();
+        this.remove(homeMenu);
+        this.add(controller);
+        this.add(view,BorderLayout.CENTER);
+        this.setUndecorated(false);
+        initialize();
+        /*to avoid problems with graphics focus controller is added here*/
+        this.addWindowFocusListener(this);
+    }
+
+    /**
+     * Enables start screen to appear
+     */
+    public void enableHomeMenu() {
+        this.dispose();
+        this.remove(instructions);
+        this.add(homeMenu,BorderLayout.CENTER);
+        this.setUndecorated(true);
+        initialize();
+        this.addWindowFocusListener(this);
+    }
+
+    private void autoLocate(){
+        Dimension size = Toolkit.getDefaultToolkit().getScreenSize();
+        int x = (size.width - this.getWidth()) / 2;
+        int y = (size.height - this.getHeight()) / 2;
+        this.setLocation(x,y);
+    }
+
+
+    @Override
+    public void windowGainedFocus(WindowEvent windowEvent) {
+        /*
+            the first time the frame loses focus is because
+            it has been disposed to install the GameBoard,
+            so went it regains the focus it's ready to play.
+            of course calling a method such as 'onLostFocus'
+            is useful only if the GameBoard as been displayed
+            at least once
+         */
+        gaming = true;
+    }
+
+    @Override
+    public void windowLostFocus(WindowEvent windowEvent) {
+        if(gaming)
+            controller.onLostFocus();
+
+    }
+}
