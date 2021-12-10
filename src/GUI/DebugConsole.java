c5c65f2a241fd1a21b7bcc70a6ef2a6372df09a2
RuiQi <ruiqi1611@gmail.com>
2021-12-10 14:35:14 +0800

    Move DebugConsole from Controller to GUI package

--- src/GUI/DebugConsole.java
+++ src/GUI/DebugConsole.java
@@ -15,11 +15,11 @@
  *  You should have received a copy of the GNU General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package Controller;
+package GUI;
 
 import Components.Ball;
 import Components.Wall;
-import GUI.GameBoardView;
+import Controller.DebugPanel;
 
 import javax.swing.*;
 import java.awt.*;
